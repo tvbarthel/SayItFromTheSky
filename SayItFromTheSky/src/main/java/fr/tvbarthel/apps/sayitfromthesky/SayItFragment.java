4817ad861f2ecd31239378ffc11d0ca25663bc5d
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-02-09 18:51:06 +0100

    Add a google map fragment

--- SayItFromTheSky/src/main/java/fr/tvbarthel/apps/sayitfromthesky/SayItFragment.java
+++ SayItFromTheSky/src/main/java/fr/tvbarthel/apps/sayitfromthesky/SayItFragment.java
@@ -7,11 +7,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+
 public class SayItFragment extends Fragment {
+
+    private MapFragment mMapFragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_say_it, container, false);
+        mMapFragment = MapFragment.newInstance(new GoogleMapOptions().mapType(GoogleMap.MAP_TYPE_NORMAL).compassEnabled(false));
+        getFragmentManager().beginTransaction().add(R.id.fragment_say_it_map_container, mMapFragment, "fragmentTagMap").commit();
         return view;
     }
+
 }
