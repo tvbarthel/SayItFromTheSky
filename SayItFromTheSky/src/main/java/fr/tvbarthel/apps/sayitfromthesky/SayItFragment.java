fbbf8b460c8589a47d99c74775ddce1b32b6e6d9
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-02-09 22:55:30 +0100

    Extend MapFragment and override onCreateView.

--- SayItFromTheSky/src/main/java/fr/tvbarthel/apps/sayitfromthesky/SayItFragment.java
+++ SayItFromTheSky/src/main/java/fr/tvbarthel/apps/sayitfromthesky/SayItFragment.java
@@ -3,24 +3,40 @@ package fr.tvbarthel.apps.sayitfromthesky;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.UiSettings;
 
-public class SayItFragment extends Fragment {
+public class SayItFragment extends Fragment implements SayItMapFragment.ISayItMapFragment {
 
-    private MapFragment mMapFragment;
+    private SayItMapFragment mMapFragment;
+    private GoogleMap mGoogleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_say_it, container, false);
-        mMapFragment = MapFragment.newInstance(new GoogleMapOptions().mapType(GoogleMap.MAP_TYPE_NORMAL).compassEnabled(false));
+        mMapFragment = new SayItMapFragment(this);
         getFragmentManager().beginTransaction().add(R.id.fragment_say_it_map_container, mMapFragment, "fragmentTagMap").commit();
         return view;
+    }
+
+    @Override
+    public void onMapReady() {
+        Log.d("argonne", "map ready !");
+        if (mGoogleMap == null) {
+            mGoogleMap = mMapFragment.getMap();
+            if (mGoogleMap != null) {
+                Log.d("argonne", "map is active !");
+                mGoogleMap.setMyLocationEnabled(true);
+                final UiSettings uiSettings = mGoogleMap.getUiSettings();
+                uiSettings.setCompassEnabled(false);
+                uiSettings.setZoomControlsEnabled(false);
+            }
+        }
     }
 
 }
---
+++ SayItFromTheSky/src/main/java/fr/tvbarthel/apps/sayitfromthesky/SayItMapFragment.java
@@ -0,0 +1,35 @@
+package fr.tvbarthel.apps.sayitfromthesky;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.MapFragment;
+
+public class SayItMapFragment extends MapFragment {
+
+    private ISayItMapFragment mInterface;
+
+    public SayItMapFragment() {
+        super();
+    }
+
+    public SayItMapFragment(ISayItMapFragment iSayItMapFragment) {
+        super();
+        mInterface = iSayItMapFragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = super.onCreateView(inflater, container, savedInstanceState);
+        if (mInterface != null) {
+            mInterface.onMapReady();
+        }
+        return v;
+    }
+
+    public interface ISayItMapFragment {
+        public void onMapReady();
+    }
+}
