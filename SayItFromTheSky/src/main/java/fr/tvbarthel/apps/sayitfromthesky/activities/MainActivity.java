da8fb1e372a0e106eab879a1acf25693752db0cd
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-09-09 13:19:10 +0200

    Add a simple parallax scrolling effect

--- SayItFromTheSky/src/main/java/fr/tvbarthel/apps/sayitfromthesky/activities/MainActivity.java
+++ SayItFromTheSky/src/main/java/fr/tvbarthel/apps/sayitfromthesky/activities/MainActivity.java
@@ -7,15 +7,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import fr.tvbarthel.apps.sayitfromthesky.R;
+import fr.tvbarthel.apps.sayitfromthesky.helpers.ActionBarHelper;
 
 public class MainActivity extends Activity {
 
+    /**
+     * Injected Views
+     */
     @InjectView(R.id.activity_main_root)
     View mRootView;
 
@@ -23,7 +28,13 @@ public class MainActivity extends Activity {
     View mHeaderContainer;
 
     @InjectView(R.id.activity_main_list_view)
-    ListView mListView;
+    ListView mObservableListView;
+
+    /**
+     * Private attributes
+     */
+    private int mActionBarSize;
+    private int mMaxHeaderTranslationY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +42,17 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
 
+        // Compute the action bar size
+        mActionBarSize = ActionBarHelper.getActionBarSize(this);
+
         // Set the height of the header container to 1/3.5 of the root height.
         ViewTreeObserver vto = mRootView.getViewTreeObserver();
         vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                final int rootHeight = mRootView.getHeight();
-                mHeaderContainer.getLayoutParams().height = (int) (rootHeight / 3.5);
+                final int headerHeight = (int) (mRootView.getHeight() / 3.5);
+                mHeaderContainer.getLayoutParams().height = headerHeight;
+                mObservableListView.setPadding(0, headerHeight, 0, 0);
                 ViewTreeObserver obs = mRootView.getViewTreeObserver();
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
                     obs.removeOnGlobalLayoutListener(this);
@@ -49,6 +64,7 @@ public class MainActivity extends Activity {
         });
 
         fakeListViewData();
+        initListView();
     }
 
 
@@ -72,11 +88,34 @@ public class MainActivity extends Activity {
     }
 
     /**
+     * Initialize the ListView.
+     * <p/>
+     * Set the onScrollListener.
+     */
+    private void initListView() {
+        mObservableListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (visibleItemCount > 0 && firstVisibleItem == 0) {
+                    final int visibleTop = mObservableListView.getPaddingTop() - mObservableListView.getChildAt(0).getTop();
+                    final int translationY = Math.max(-visibleTop / 2, -mActionBarSize);
+                    mHeaderContainer.setTranslationY(translationY);
+                }
+            }
+        });
+
+    }
+
+    /**
      * TODO remove
      * test purpose only.
      */
     private void fakeListViewData() {
-        mListView.setAdapter(new ArrayAdapter<String>(this,
+        mObservableListView.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
                 java.util.Arrays.asList("string 1",
                         "string 2",
