4b83a8b99c30bfb9c8f63c676c06250654972412
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-09-09 09:18:01 +0200

    Remove the navigation drawer

--- SayItFromTheSky/src/main/java/fr/tvbarthel/apps/sayitfromthesky/activities/MainActivity.java
+++ SayItFromTheSky/src/main/java/fr/tvbarthel/apps/sayitfromthesky/activities/MainActivity.java
@@ -1,131 +1,25 @@
 package fr.tvbarthel.apps.sayitfromthesky.activities;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
 import fr.tvbarthel.apps.sayitfromthesky.R;
-import fr.tvbarthel.apps.sayitfromthesky.fragments.MyPathFragment;
-import fr.tvbarthel.apps.sayitfromthesky.fragments.NavigationDrawerFragment;
-import fr.tvbarthel.apps.sayitfromthesky.fragments.SayItFragment;
 
-public class MainActivity extends FragmentActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
-
-    private static final String FRAGMENT_TAG_SAY_IT = "MainActivity.Fragment.Tag.SayIt";
-    private static final String FRAGMENT_TAG_MY_PATHS = "MainActivity.Fragment.Tag.MyPaths";
-    private static final String TRANSACTION_STATE_NAME_PATH_DETAIL = "MainActivity.Transaction.Name.PathDetail";
-
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-    }
-
-    @Override
-    public void onNavigationDrawerItemSelected(int position, boolean isUserAction) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        //TODO remove placeholder fragments
-        if (position <= 2) {
-            fragmentManager.popBackStack(TRANSACTION_STATE_NAME_PATH_DETAIL, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-            fragmentManager.beginTransaction()
-                    .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                    .commit();
-        } else if (position == 3) {
-            fragmentManager.popBackStack(TRANSACTION_STATE_NAME_PATH_DETAIL, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-            SayItFragment sayItFragment = (SayItFragment) fragmentManager.findFragmentByTag(FRAGMENT_TAG_SAY_IT);
-            if (sayItFragment == null) {
-                sayItFragment = new SayItFragment();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.container, sayItFragment, FRAGMENT_TAG_SAY_IT)
-                        .commit();
-            } else {
-                fragmentManager.beginTransaction()
-                        .show(sayItFragment).commit();
-            }
-            //TODO don't use hard coded string
-            mTitle = "Say It";
-        } else if (position == 4) {
-
-            MyPathFragment myPathFragment = (MyPathFragment) fragmentManager.findFragmentByTag(FRAGMENT_TAG_MY_PATHS);
-            if (myPathFragment == null) {
-                myPathFragment = new MyPathFragment();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.container, myPathFragment, FRAGMENT_TAG_MY_PATHS)
-                        .commit();
-            } else {
-                fragmentManager.beginTransaction()
-                        .show(myPathFragment).commit();
-            }
-            //TODO don't use hard coded string
-            mTitle = "My Paths";
-        }
-
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.global, menu);
+        return true;
     }
 
     @Override
@@ -139,47 +33,4 @@ public class MainActivity extends FragmentActivity
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
 }
